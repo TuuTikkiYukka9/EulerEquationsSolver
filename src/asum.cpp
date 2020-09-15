7b8be5e8bae083a89684b92c2544488ef52d69b7
Rusakova.J <julija.lt@mail.ru>
2020-09-16 01:58:30 +0300

    creating functions in the ASUM module

--- src/asum.cpp
+++ src/asum.cpp
@@ -38,8 +38,6 @@ void ASUM::solve() {
 	Matrix<double> u(nk, n1);
 	Matrix<double> p(nk, n1);
 
-	double a_L, a_R, M_L, M_R;
-
 	Matrix<consv> U(nk, n1);
 	Matrix<potoc> F(nk, n1);
 	Matrix<consv> U_drob(nk, n1);
@@ -49,46 +47,20 @@ void ASUM::solve() {
 	p.SetAllElements(0);
 	u.SetAllElements(0);
 
-	double m_plus, m_minus;
-	double p_plus, p_minus;
+	Variables left, right;
+	potoc F_plus, F_minus;
 
-	double p_L = bcLeft.p, ro_L = bcLeft.ro, u_L = bcLeft.u;//вот это надо считывать из файла
-	double p_R = bcRight.p, ro_R = bcRight.ro, u_R = bcRight.u;
-	double x_0 = x0;
-	double currentCoordinate;
-	bool isLeft;
-
+	
 	//начальное условие
-	for (int i = 0; i < p.height(); i++) {
-		currentCoordinate = minX + (h*i);
-		if (currentCoordinate == x_0) {
-			a_L = sqrt(1.4*p_L / ro_L);
-			M_L = u_L / a_L;
-			m_plus = (fabs(M_L) <= 1) ? (0.25*pow((M_L + 1), 2)) : (0.5*(M_L + fabs(M_L)));
-			a_R = sqrt(1.4*p_R / ro_R);
-			M_R = u_R / a_R;
-			m_minus = (fabs(M_R) <= 1) ? (-0.25*pow((M_R - 1), 2)) : (0.5*(M_R - fabs(M_R)));
-		}
-		
-		isLeft = currentCoordinate < x_0 || (currentCoordinate == x_0 && (m_plus + m_minus) >= 0);
-		
-		p[0][i] = isLeft ? p_L : p_R;
-		ro[0][i] = isLeft ? ro_L : ro_R;
-		u[0][i] = isLeft ? u_L : u_R;
-	}
+	setInitialConditions(ro[0], u[0], p[0], x0, bcLeft, bcRight);
 
 	//граничные условия
 	for (int k = 0; k < p.height(); k++) {
-		ro[k].setFirst(ro_L);
-		u[k].setFirst(u_L);
-		p[k].setFirst(p_L);
-		ro[k].setLast(ro_R);
-		u[k].setLast(u_R);
-		p[k].setLast(p_R);
+		setBoundaryConditions(ro[k], u[k], p[k], bcLeft, bcRight);
 	}
 
 	for (int k = 0; k < p.height(); k++) {
-		for (int i = 0;i<p.width();i++) {
+		for (int i = 0; i < p.width(); i++) {
 			U[k][i].U1 = ro[k][i];
 			U[k][i].U2 = ro[k][i] * u[k][i];
 			U[k][i].U3 = (p[k][i] / (1.4 - 1.0)) + 0.5*ro[k][i] * pow(u[k][i], 2);
@@ -101,59 +73,30 @@ void ASUM::solve() {
 
 	for (int k = 0; k < p.height() - 1; k++) {
 		for (int i = 1; i < p.width() - 1; i++) {
-								   //_________________________AUSM_______________________________//			
-				struct potoc F_plus, F_minus, calculetedFlow;
+			//_________________________AUSM_______________________________//	
 
-				for (int iter = 0; iter<2;iter++) {
-					int j = i + iter;
-					p_L = p[k][j - 1];//ro
-					ro_L = ro[k][j - 1];// ro8u
-					u_L = u[k][j - 1];//E
-
-					p_R = p[k][j];//ro
-					ro_R = ro[k][j];// ro8u
-					u_R = u[k][j];//E
-
-					a_L = sqrt(1.4*p_L / ro_L);
-					M_L = u_L / a_L;
-					m_plus = fabs(M_L) <= 1 ? 0.25*pow((M_L + 1), 2) : 0.5*(M_L + fabs(M_L));
-					p_plus = fabs(M_L) <= 1 ?  0.5*p_L*(1 + M_L) : 0.5*p_L*((M_L + fabs(M_L)) / M_L);
+			for (int iter = 0; iter < 2; iter++) {
+				int j = i + iter;
 					
+				left = { ro[k][j - 1], u[k][j - 1], p[k][j - 1] };
+				right = { ro[k][j], u[k][j], p[k][j] };
 
-					a_R = sqrt(1.4*p_R / ro_R);
-					M_R = u_R / a_R;
-					m_minus = fabs(M_R) <= 1 ? -0.25*pow((M_R - 1), 2) : 0.5*(M_R - fabs(M_R));
-					p_minus = fabs(M_R) <= 1 ? 0.5*p_R*(1 - M_R) : 0.5*p_R*((M_R - fabs(M_R)) / M_R);;
-					
-
-					if ((m_plus + m_minus) >= 0) {
-						calculetedFlow.F1 = (m_plus + m_minus)*ro_L*a_L + 0;
-						calculetedFlow.F2 = (m_plus + m_minus)*ro_L*u_L*a_L + (p_plus + p_minus);
-						calculetedFlow.F3 = (m_plus + m_minus)*(p_L + (p_L / 0.4 + (ro_L*pow(u_L, 2) / 2)))*a_L + 0;
-					}
-					else {
-						calculetedFlow.F1 = (m_plus + m_minus)*ro_R*a_R + 0;
-						calculetedFlow.F2 = (m_plus + m_minus)*ro_R*u_R*a_R + (p_plus + p_minus);
-						calculetedFlow.F3 = (m_plus + m_minus)*(p_R + (p_R / 0.4 + (ro_R*pow(u_R, 2) / 2)))*a_R + 0;
-					}
-
-					if (iter == 0) {
-						F_minus = calculetedFlow;
-					}
-					else {
-						F_plus = calculetedFlow;
-					}
+				if (iter == 0) {
+					F_minus = calcFlow(left, right);
 				}
+				else {
+					F_plus = calcFlow(left, right);
+				}
+			}
 
-
-				U[k + 1][i].U1 = U[k][i].U1 - (tau / h)*(F_plus.F1 - F_minus.F1);
-				U[k + 1][i].U2 = U[k][i].U2 - (tau / h)*(F_plus.F2 - F_minus.F2);
-				U[k + 1][i].U3 = U[k][i].U3 - (tau / h)*(F_plus.F3 - F_minus.F3);
-				//_____________________________end AUSM__________________________________//
+			U[k + 1][i].U1 = U[k][i].U1 - (tau / h)*(F_plus.F1 - F_minus.F1);
+			U[k + 1][i].U2 = U[k][i].U2 - (tau / h)*(F_plus.F2 - F_minus.F2);
+			U[k + 1][i].U3 = U[k][i].U3 - (tau / h)*(F_plus.F3 - F_minus.F3);
+			//_____________________________end AUSM__________________________________//
 
 		}
 
-		for (int i = 1;i< p.width() - 1;i++) {
+		for (int i = 1; i < p.width() - 1; i++) {
 			ro[k + 1][i] = U[k + 1][i].U1;
 			u[k + 1][i] = U[k + 1][i].U2 / U[k + 1][i].U1;
 			p[k + 1][i] = (1.4 - 1.0)*(U[k + 1][i].U3 - (pow(U[k + 1][i].U2, 2)) / (2.0*U[k + 1][i].U1));
@@ -168,6 +111,92 @@ void ASUM::solve() {
 	(new SolutionWriter())->write("AUSM", ro[nk - 1], u[nk - 1], p[nk - 1]);
 }
 
+double ASUM::calcSpeedOfSound(Variables var){
+	double gamma = 1.4;
+	return sqrt(gamma * var.p / var.ro);
+}
+
+double ASUM::calcMachNumber(Variables var) {
+	return var.u / calcSpeedOfSound(var);
+}
+
+double ASUM::m_plus(Variables var) {
+	return (fabs(calcMachNumber(var)) <= 1) ? 
+		     (0.25*pow((calcMachNumber(var) + 1), 2)) : 
+		   (0.5*(calcMachNumber(var) + fabs(calcMachNumber(var))));
+}
+double ASUM::m_minus(Variables var) {
+	return (fabs(calcMachNumber(var)) <= 1) ? 
+		    (-0.25*pow((calcMachNumber(var) - 1), 2)) : 
+		  (0.5*(calcMachNumber(var) - fabs(calcMachNumber(var))));
+}
+double ASUM::p_plus(Variables var) {
+	return fabs(calcMachNumber(var)) <= 1 ? 
+		   0.5*var.p*(1 + calcMachNumber(var)) :
+		   0.5*var.p*((calcMachNumber(var) + fabs(calcMachNumber(var))) / calcMachNumber(var));
+}
+double ASUM::p_minus(Variables var) {
+	return fabs(calcMachNumber(var)) <= 1 ? 
+		   0.5*var.p*(1 - calcMachNumber(var)) :
+		   0.5*var.p*((calcMachNumber(var) - fabs(calcMachNumber(var))) / calcMachNumber(var));
+}
+
+
+double ASUM::calcBorderMachNumber(Variables &left, Variables &right){
+	return m_plus(left) + m_minus(right);
+}
+double ASUM::calcBorderPressure(Variables &left, Variables &right) {
+	return p_plus(left) + p_minus(right);
+}
+potoc ASUM::calcFlow(Variables var, double borderMachNumber, double borderPressure)
+{
+	potoc flow;
+	flow.F1 = borderMachNumber * var.ro * calcSpeedOfSound(var) + 0;
+	flow.F2 = borderMachNumber * var.ro * var.u * calcSpeedOfSound(var) + borderPressure;
+	flow.F3 = borderMachNumber * (var.p + (var.p / 0.4 + (var.ro * pow(var.u, 2) / 2))) * calcSpeedOfSound(var) + 0;
+	return flow;
+}
+potoc ASUM::calcFlow(Variables &left, Variables &right)
+{
+	double borderMachNumber = calcBorderMachNumber(left, right);
+	double borderPressure = calcBorderPressure(left, right);
+	return (borderMachNumber >= 0) ?
+		   calcFlow(left, borderMachNumber, borderPressure) :
+		   calcFlow(right, borderMachNumber, borderPressure);
+}
+
+void ASUM::setBoundaryConditions(Array<double> &ro, Array<double> &u, Array<double> &p,
+	Variables &left, Variables &right) {
+	ro.setFirst(left.ro);
+	u.setFirst(left.u);
+	p.setFirst(left.p);
+
+	ro.setLast(right.ro);
+	u.setLast(right.u);
+	p.setLast(right.p);
+}
+
+void ASUM::setInitialConditions(Array<double> &ro, Array<double> &u, Array<double> &p,
+	double x0, Variables &left, Variables &right) {
+	
+	double currentCoordinate;
+	bool isLeft;
+
+	double borderMachNumber;
+
+	 for (int i = 0; i < p.length(); i++) {
+		currentCoordinate = minX + (h*i);
+		if (currentCoordinate == x0) {
+			borderMachNumber = calcBorderMachNumber(left, right);
+		}
+
+		isLeft = currentCoordinate < x0 || (currentCoordinate == x0 && borderMachNumber >= 0);
+
+		p[i] = isLeft ? left.p : right.p;
+		ro[i] = isLeft ? left.ro : right.ro;
+		u[i] = isLeft ? left.u: right.u;
+	}
+}
 
 ASUM::~ASUM()
 {
--- src/asum.h
+++ src/asum.h
@@ -1,11 +1,26 @@
 #pragma once
 #include "Solver.h"
+#include "array.h"
 
 class ASUM : public Solver
 {
+private:
+	double calcSpeedOfSound(Variables var);
+	double calcMachNumber(Variables var);
+	double m_plus(Variables var);
+	double m_minus(Variables var);
+	double p_plus(Variables var);
+	double p_minus(Variables var);
+	double calcBorderMachNumber(Variables &left, Variables &right);
+	double calcBorderPressure(Variables &left, Variables &right);
+	potoc calcFlow(Variables var, double borderMachNumber, double borderPressure);
+	potoc calcFlow(Variables &left, Variables &right);
 public:
 	ASUM();
 	~ASUM();
 	void solve();
+	void setBoundaryConditions(Array<double> &ro, Array<double> &u, Array<double> &p, Variables &left, Variables &right);
+	void setInitialConditions(Array<double> &ro, Array<double> &u, Array<double> &p, double x0, Variables &left, Variables &right);
+
 };
 
